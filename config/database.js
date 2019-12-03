67f1f29dabaaf974b70f4653e1352066fbe9ffa4
孙星恒 <361789874@qq.com>
2019-12-03 17:34:23 +0800

    链接数据库

---
+++ config/database.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');// 创建 mysql 连接池资源
+var pool = mysql.createPool({
+    host: '139.199.198.173',
+    user: 'root',
+    password: '123456',
+    database: 'mysql',
+    port: 3306
+});
+
+exports.query = function (sql, arr, callback) {
+    //建立链接
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            throw err;
+            return;
+        }
+        connection.query(sql, arr, function (error, results, fields) {
+            //将链接返回到连接池中，准备由其他人重复使用
+            connection.release();
+            if (error) throw error;            //执行回调函数，将数据返回
+            callback && callback(results, fields);
+        });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var app = express();
+var db = require('../config/database')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  // console.log()
+  db.query("select * from db ",function(err,vals,fields){
+    //其他信息
+    res.render('index', { title: 'Express' });
+  });
+  // db.query("SELECT * FROM db; ",[],function(result,fields){
+  //   return 'xxx'
+  // })
 
+
+
+
+
+});
 module.exports = router;
